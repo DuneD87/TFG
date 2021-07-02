0bae16238c125a95947af8e198e3e641f5d359fe
mr.Xavier <ventueshomo@caca.com>
2021-07-02 11:28:56 +0200

    Shader tutorial - Basic ( object rendering, textures, transform i cameras ) Phong Lighting implemented PointLights, DirectionalLights and SpotLights implemented Model loading interface implemented, cleaned code

--- main.cpp
+++ main.cpp
@@ -194,48 +194,27 @@ int main()
             -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
-    unsigned int VBO, cubeVao;
-    glGenVertexArrays(1, &cubeVao);
+    unsigned int VBO;
     glGenBuffers(1, &VBO);
 
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindVertexArray(cubeVao);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // normal attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
     unsigned int lightCubeVao;
     glGenVertexArrays(1, &lightCubeVao);
     glBindVertexArray(lightCubeVao);
 
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    unsigned int diffuseMap = loadTexture("../Textures/container2.png");
-    unsigned int specularMap = loadTexture("../Textures/container2_specular.png");
+
     lightShader.use();
     lightShader.setInt("material.diffuse", 0);
     lightShader.setInt("material.specular", 1);
     //Render loop
-    glm::vec3 cubePositions[] = {
-            glm::vec3( 0.0f,  0.0f,  0.0f),
-            glm::vec3( 2.0f,  5.0f, -15.0f),
-            glm::vec3(-1.5f, -2.2f, -2.5f),
-            glm::vec3(-3.8f, -2.0f, -12.3f),
-            glm::vec3( 2.4f, -0.4f, -3.5f),
-            glm::vec3(-1.7f,  3.0f, -7.5f),
-            glm::vec3( 1.3f, -2.0f, -2.5f),
-            glm::vec3( 1.5f,  2.0f, -2.5f),
-            glm::vec3( 1.5f,  0.2f, -1.5f),
-            glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
+
     glm::vec3 pointLightPositions[] = {
             glm::vec3( 0.7f,  0.2f,  2.0f),
             glm::vec3( 2.3f, -3.3f, -4.0f),
@@ -318,29 +297,12 @@ int main()
         glm::mat4 model = glm::mat4(1.0f);
         lightShader.setMat4("model", model);
         backpack.Draw(lightShader);
-        // bind diffuse map
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, diffuseMap);
-        // bind specular map
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, specularMap);
-        glBindVertexArray(cubeVao);
-        for(unsigned int i = 0; i < 10; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            lightShader.setMat4("model", model);
-
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
 
 
         cubeLightShader.use();
         cubeLightShader.setMat4("projection", projection);
         cubeLightShader.setMat4("view", view);
-        glBindVertexArray(cubeVao);
+        glBindVertexArray(lightCubeVao);
         for (int i = 0 ; i < 4; i++) {
             model = glm::mat4(1.0f);
             model = glm::translate(model, pointLightPositions[i]);
@@ -360,7 +322,6 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &cubeVao);
     glDeleteVertexArrays(1, &lightCubeVao);
     glDeleteBuffers(1, &VBO);
 
